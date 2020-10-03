d644b99b0d472d4012ece47465c65b4ff474e9b8
vladimir3700 <70822300+vladimir3700@users.noreply.github.com>
2020-10-03 12:25:15 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array)
+{
+  if  ((!array) || (array.length === 0))
+    return 0;
+    else 
+    {
+    return Math.min.apply(null, array)
+    }
+  
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array) 
+{
+  if ((!array) || (array.length === 0))
+    return 0;
+  else 
+  {
+    return Math.max.apply(null, array)
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) 
+{
+  if ((!array) || (array.length === 0))
+    return 0;
+  else 
+  {
+    return array.reduce((accumulator, currentValue) => (accumulator + currentValue), 0) / array.length;
+  }
 }
